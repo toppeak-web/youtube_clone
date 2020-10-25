375767f70bbe7b5bd3bb5462fb200e389553e256
toppeak <xkqvor123@gmail.com>
2020-10-25 17:10:28 +0900

    async fuctiocn change 비동기 함수로 전환

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -6,19 +6,17 @@ class Youtube {
             redirect: 'follow'
         };
     }
-    mostPopular() {
-        return fetch(`https://www.googleapis.com//youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=${this.key}`,
-            this.getRequestOptions)
-            .then(response => response.json())
-            .then(result => result.items)
+    async mostPopular() {
+        const response = await fetch(`https://www.googleapis.com//youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=${this.key}`,
+            this.getRequestOptions);
+        const result = await response.json();
+        return result.items;
     }
-    search(q) {
-        return fetch(`https://www.googleapis.com//youtube/v3/search?part=snippet&maxResults=25&q=${q}&type=video&key=${this.key}`,
-            this.getRequestOptions)
-            .then(response => response.json())
-            .then(result =>
-                result.items.map(item => ({ ...item, id: item.id.videoId }))
-            )
+    async search(q) {
+        const response = await fetch(`https://www.googleapis.com//youtube/v3/search?part=snippet&maxResults=25&q=${q}&type=video&key=${this.key}`,
+            this.getRequestOptions);
+        const result = await response.json();
+        return result.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 
 }
