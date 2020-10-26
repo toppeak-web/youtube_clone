c22a1cdcfc3dab14d6c41ef07d7de52329ac53b2
toppeak <xkqvor123@gmail.com>
2020-10-26 09:42:06 +0900

    little fix 약간 수정

--- src/components/video_detail/Video_detail.jsx
+++ src/components/video_detail/Video_detail.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from "./Video_detail module.css"
 
 const VideoDetail = ({ video, video: {snippet }}) => (
     <>
-        <section>
+        <section className={styles.detail}>
             <iframe id="ytplayer" type="text/html" width="1080" height="607.5"
                 src={`https://www.youtube.com/embed/${video.id}`}
-                frameborder="0" allowfullscreen />
+                frameborder="0" allowfullscreen="true"/>
         </section>
         {console.log(video)}
         <h2>{snippet.title}</h2>
