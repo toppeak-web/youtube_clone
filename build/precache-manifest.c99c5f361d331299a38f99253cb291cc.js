1239f6af9d491719e1863bad2f67b79e2e5bcf6b
toppeak <xkqvor123@gmail.com>
2020-10-29 15:59:51 +0900

    depoly 배포

---
+++ build/precache-manifest.c99c5f361d331299a38f99253cb291cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ba40449e2d333f3999a8e0fa00918d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a9f24ae75ebe33a8630",
+    "url": "./static/css/main.56071ddb.chunk.css"
+  },
+  {
+    "revision": "72c2e5248ead7d546dd0",
+    "url": "./static/js/2.c26c4b5b.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "./static/js/2.c26c4b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9f24ae75ebe33a8630",
+    "url": "./static/js/main.59f1339b.chunk.js"
+  },
+  {
+    "revision": "70bfe9043cb71be8a31c",
+    "url": "./static/js/runtime-main.83775e78.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9860858ae262d248229d977756baf40e.js"
+  "./precache-manifest.c99c5f361d331299a38f99253cb291cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
